46a01a368cec1e52db71a7639fd5569b3b4d8e71
Olev Mutso <olev.mutso@resys.io>
2021-08-03 11:46:10 +0300

    test-printer

---
+++ thena-core/thena-docdb-api/src/main/java/io/resys/thena/docdb/spi/DocDBTestPrinter.java
@@ -0,0 +1,122 @@
+package io.resys.thena.docdb.spi;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import io.resys.thena.docdb.api.models.Repo;
+
+public class DocDBTestPrinter {
+  private final ClientState state;
+
+  public DocDBTestPrinter(ClientState state) {
+    super();
+    this.state = state;
+  }
+
+  public String print(Repo repo) {
+    final Map<String, String> replacements = new HashMap<>();
+    final Function<String, String> ID = (id) -> {
+      if(replacements.containsKey(id)) {
+        return replacements.get(id);
+      }
+      final var next = String.valueOf(replacements.size() + 1);
+      replacements.put(id, next);
+      return next;
+    };
+
+    final var ctx = state.withRepo(repo);
+    
+    StringBuilder result = new StringBuilder();
+
+    result
+    .append(System.lineSeparator())
+    .append("Repo").append(System.lineSeparator())
+    .append("  - id: ").append(ID.apply(repo.getId()))
+    .append(", rev: ").append(ID.apply(repo.getRev())).append(System.lineSeparator())
+    .append("    name: ").append(repo.getName())
+    .append(", prefix: ").append(repo.getPrefix()).append(System.lineSeparator());
+    
+    result
+    .append(System.lineSeparator())
+    .append("Refs").append(System.lineSeparator());
+    
+    ctx.query().refs()
+    .find().onItem()
+    .transform(item -> {
+      result.append("  - ")
+      .append(ID.apply(item.getCommit())).append(": ").append(item.getName())
+      .append(System.lineSeparator());
+      return item;
+    }).collectItems().asList().await().indefinitely();
+
+    
+    result
+    .append(System.lineSeparator())
+    .append("Tags").append(System.lineSeparator());
+    
+    ctx.query().tags()
+    .find().onItem()
+    .transform(item -> {
+      result.append("  - id: ").append(item.getName())
+      .append(System.lineSeparator())
+      .append("    commit: ").append(ID.apply(item.getCommit()))
+      .append(", message: ").append(item.getMessage())
+      .append(System.lineSeparator());
+      
+      return item;
+    }).collectItems().asList().await().indefinitely();
+    
+    result
+    .append(System.lineSeparator())
+    .append("Commits").append(System.lineSeparator());
+    
+    ctx.query().commits()
+    .find().onItem()
+    .transform(item -> {
+      result.append("  - id: ").append(ID.apply(item.getId()))
+      .append(System.lineSeparator())
+      .append("    tree: ").append(ID.apply(item.getTree()))
+      .append(", parent: ").append(item.getParent().map(e -> ID.apply(e)).orElse(""))
+      .append(", message: ").append(item.getMessage())
+      .append(System.lineSeparator());
+      
+      return item;
+    }).collectItems().asList().await().indefinitely();
+    
+    
+    result
+    .append(System.lineSeparator())
+    .append("Trees").append(System.lineSeparator());
+    
+    ctx.query().trees()
+    .find().onItem()
+    .transform(item -> {
+      result.append("  - id: ").append(ID.apply(item.getId())).append(System.lineSeparator());
+      item.getValues().entrySet().forEach(e -> {
+        result.append("    ")
+          .append(e.getValue().getBlob())
+          .append(": ")
+          .append(e.getValue().getName())
+          .append(System.lineSeparator());
+      });
+      
+      return item;
+    }).collectItems().asList().await().indefinitely();
+    
+    
+    
+    result
+    .append(System.lineSeparator())
+    .append("Blobs").append(System.lineSeparator());
+    
+    ctx.query().blobs()
+    .find().onItem()
+    .transform(item -> {
+      result.append("  - ").append(item.getId()).append(": ").append(item.getValue()).append(System.lineSeparator());
+      return item;
+    }).collectItems().asList().await().indefinitely();
+    
+    return result.toString();
+  }
+}
