41bf316037ec2a6b09b668f87160e26e167a66c7
jocelyn.mutso <jocelyn.mutso@resys.io>
2023-07-07 16:38:38 +0300

    add dev tab to ui for component development

---
+++ extensions/tasks-parent/tasks-ui/src/core/Dev/Buttons.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Button, Typography, Stack } from '@mui/material';
+import Burger from '@the-wrench-io/react-burger';
+
+
+const Buttons: React.FC<{}> = () => {
+
+  return (
+
+    <Box>
+      <Typography sx={{ mb: 1 }}>Burger buttons</Typography>
+      <Stack direction="row" spacing={2} sx={{ mb: 1 }}>
+        <Burger.PrimaryButton label='PRIMARY' onClick={() => { }} />
+        <Burger.SecondaryButton label='SECONDARY' onClick={() => { }} />
+      </Stack>
+    </Box>)
+}
+
+
+export default Buttons;
--- extensions/tasks-parent/tasks-ui/src/core/Dev/Dev.tsx
+++ extensions/tasks-parent/tasks-ui/src/core/Dev/Dev.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import { Box, TableContainer, Table, TablePagination } from '@mui/material';
-
-import client from '@taskclient';
+import Buttons from './Buttons';
 
 const Dev: React.FC<{}> = () => {
 
 
-  return (<>DEVaurlaurlaurlauraru</>);
+  return (<Box sx={{ width: '100%', p: 1 }}>
+    <Buttons />
+
+
+
+  </Box>);
 }
 
 export { Dev };
