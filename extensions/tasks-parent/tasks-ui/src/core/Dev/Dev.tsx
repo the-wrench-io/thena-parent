aca2bdd56714aa7db00d306bdcfaa569cada002b
jocelyn.mutso <jocelyn.mutso@resys.io>
2023-07-10 14:52:54 +0300

    #52 initial Dialog and fields concept

--- extensions/tasks-parent/tasks-ui/src/core/Dev/Dev.tsx
+++ extensions/tasks-parent/tasks-ui/src/core/Dev/Dev.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { FormattedMessage } from 'react-intl';
+
 import Buttons from './Buttons';
-import Help from './Help';
+import Dialog from './Dialog';
+import { DateField, TextField } from './Fields';
+
 
 const Dev: React.FC = () => {
 
 
-  return (<Box sx={{ width: '100%', p: 1 }}>
-    <Buttons />
+  return (
+    <Box sx={{ width: '100%', p: 1 }}>
+      <Buttons />
+      <Box sx={{ m: 1 }} />
+      <Dialog />
+      <Box sx={{ m: 1 }} />
+      <Typography><FormattedMessage id='fields.dueDate' /></Typography>
+      <DateField label='fields.dueDate'/>
+      <Box sx={{ m: 1 }} />
+      <Typography><FormattedMessage id='fields.textField' /></Typography>
+      <TextField label='fields.textField' />
 
-
-
-  </Box>);
+    </Box>);
 }
 
 export { Dev };
