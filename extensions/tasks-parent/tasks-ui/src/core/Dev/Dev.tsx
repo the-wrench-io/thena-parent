c8305072ef5126ee2b6b9f9d894db3a9abe0420e
Admir Krilašević <admirkrilasevic@gmail.com>
2023-07-19 12:46:56 +0200

    custom date picker (#23)

--- extensions/tasks-parent/tasks-ui/src/core/Dev/Dev.tsx
+++ extensions/tasks-parent/tasks-ui/src/core/Dev/Dev.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Box, Divider, Button } from '@mui/material';
 import TaskOps from '../TaskOps';
 import TestTask from './test_task_1.json';
-
+import DatePicker from '../DatePicker';
 
 const Dev: React.FC = () => {
 
@@ -18,8 +18,10 @@ const Dev: React.FC = () => {
       <Divider />
       <Box>Edit task dialog</Box>
       <Button variant='contained' onClick={() => setOpen(true)}>Open dialog</Button>
-      
-      
+
+      <Divider sx={{ my: 2 }} />
+      <DatePicker />
+
       <TaskOps.EditTaskDialog onClose={() => setOpen(false)} open={open} task={TestTask as any} />
 
     </Box>);
