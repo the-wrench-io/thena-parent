a42eb40a0abd8bc88c98aecf601dce4a46dab486
Tomi Pakarinen <tomi.pakarinen@resys.io>
2024-09-06 14:54:26 +0300

    Refactored build scripts. Use actions/setup-java@v4 and actions/checkout@v4

--- build-parent/release.sh
+++ build-parent/release.sh
@@ -1,5 +1,29 @@
+#
+# Copyright © 2015 - 2021 ReSys (info@dialob.io)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 #!/usr/bin/env bash
 set -e
+
+run_build() {
+  # https://issues.sonatype.org/browse/NEXUS-27902
+  export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED"
+  ./mvnw clean deploy -Pthena-release
+}
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # No changes, skip release
 readonly local last_release_commit_hash=$(git log --author="$BOT_NAME" --pretty=format:"%H" -1)
@@ -10,51 +34,41 @@ if [[ "${last_release_commit_hash}" = "${GITHUB_SHA}" ]]; then
      #exit 0
 fi
 
-echo "Import GPG key"
-echo "$GPG_KEY" > private.key
-gpg --batch --import ./private.key 
-rm ./private.key
-echo "JAVA_HOME '$JAVA_HOME'"
-
 # Config GIT
 echo "Setup git user name to '$BOT_NAME' and email to '$BOT_EMAIL' GPG key ID $GPG_KEY_ID"
 git config --global user.name "$BOT_NAME";
 git config --global user.email "$BOT_EMAIL";
 
-# Checkout
-git reset --hard
-git fetch --all
-git branch -a --contains ${GITHUB_SHA} --format="%(refname)"
-
-readonly local refname=$(git branch -a --contains ${GITHUB_SHA} --format="%(refname)" | head -1)
-if [[ "${refname}" = "refs/heads/main" ]]; then
-     readonly local branch="main"
-else
-     readonly local branch=${refname#refs/remotes/origin/}
-fi
+echo "Git checkout refname: '${refname}' branch: '${branch}' commit: '${GITHUB_SHA}'"
 
 # Current and next version
-LAST_RELEASE_VERSION=$(cat thena-build-parent/release.version)
-[[ $LAST_RELEASE_VERSION =~ ([^\\.]*)$ ]]
-MINOR_VERSION=`expr ${BASH_REMATCH[1]} + 1`
-MAJOR_VERSION=${LAST_RELEASE_VERSION:0:`expr ${#LAST_RELEASE_VERSION} - ${#MINOR_VERSION}`}
-RELEASE_VERSION=${MAJOR_VERSION}${MINOR_VERSION}
-echo ${RELEASE_VERSION} > thena-build-parent/release.version
+RELEASE_VERSION=$(cat $SCRIPT_DIR/next-release.version | xargs)
+if [[ $RELEASE_VERSION =~ ([0-9]+)$ ]]; then
+  MINOR_VERSION=${BASH_REMATCH[1]}
+  echo "Releasing   : '${RELEASE_VERSION}'"
+  MAJOR_VERSION=${RELEASE_VERSION:0:`expr ${#RELEASE_VERSION} - ${#MINOR_VERSION}`}
+  NEW_MINOR_VERSION=`expr ${MINOR_VERSION} + 1`
+  NEXT_RELEASE_VERSION=${MAJOR_VERSION}${NEW_MINOR_VERSION}
+  echo "Next version: '${NEXT_RELEASE_VERSION}'"
+else
+  echo "Could not parse version : '$RELEASE_VERSION'"
+  exit 1
+fi
 
-PROJECT_VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+echo "${NEXT_RELEASE_VERSION}" > $SCRIPT_DIR/next-release.version
 
-echo "Git checkout refname: '${refname}' branch: '${branch}' commit: '${GITHUB_SHA}'"
+PROJECT_VERSION=$(./mvnw -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
 echo "Dev version: '${PROJECT_VERSION}' release version: '${RELEASE_VERSION}'"
-echo "Releasing: '${RELEASE_VERSION}', previous: '${LAST_RELEASE_VERSION}'"
-mvn -version
 
-git checkout ${branch}
-mvn versions:set -DnewVersion=${RELEASE_VERSION}
-git commit -am "release: ${RELEASE_VERSION}"
+./mvnw versions:set -DnewVersion=${RELEASE_VERSION}
+git commit -am "Release ${RELEASE_VERSION}"
 git tag -a ${RELEASE_VERSION} -m "release ${RELEASE_VERSION}"
 
-mvn clean deploy -Pthena-release --settings thena-build-parent/ci-maven-settings.xml
-mvn versions:set -DnewVersion=${PROJECT_VERSION}
-git commit -am "release: ${RELEASE_VERSION}"
+run_build
+
+./mvnw versions:set -DnewVersion=${PROJECT_VERSION}
+git commit -am "Prepare ${NEXT_RELEASE_VERSION} development"
 git push
 git push origin ${RELEASE_VERSION}
+
+echo "### Version ${RELEASE_VERSION} release build" >> $GITHUB_STEP_SUMMARY
